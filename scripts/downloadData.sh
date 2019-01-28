eb39242e3d1f087abbb3946592b5dc92a144b32d
Stefan van Gastel <stefanvangastel@gmail.com>
2019-01-28 07:44:27 +0100

    fix redirect

--- scripts/downloadData.sh
+++ scripts/downloadData.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -o data/green_tripdata_2019-04.csv https://www.dropbox.com/s/io0llunlr03qhze/green_tripdata_2019-04.csv?dl=1
+curl -L -o data/green_tripdata_2019-04.csv https://www.dropbox.com/s/io0llunlr03qhze/green_tripdata_2019-04.csv?dl=1
